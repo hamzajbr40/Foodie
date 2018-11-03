b1990d83559d996be5500442360ac28a9ad5fbd8
Hekmat <hekmatshak@yahoo.com>
2018-11-03 22:59:19 +0200

    I made a mirror object from the skip button and then i make a clickListener that will execute a user defined method that is called openCategoryActivity and by that you'll be moved to the category activity.(Hekmat)

--- app/src/main/java/com/dev40/foodie/Category_Activity.java
+++ app/src/main/java/com/dev40/foodie/Category_Activity.java
@@ -8,6 +8,6 @@ public class Category_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category_);
+        setContentView(R.layout.activity_category);
     }
 }

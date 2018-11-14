ea72c619c3202b541f3d7b34b7f61e793eaf0fe3
Dev-40 <44698972+Dev-40@users.noreply.github.com>
2018-11-14 12:44:47 +0200

    Edit in login and sign up design

--- app/src/main/java/com/dev40/foodie/SignUpActivity.java
+++ app/src/main/java/com/dev40/foodie/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.dev40.foodie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signUp(View view) {
+        Toast.makeText(this,"Coming Soon", Toast.LENGTH_LONG).show();
     }
 }

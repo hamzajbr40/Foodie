1e5d7ce60bb0f982baee8f94d6f7c67738e36ba4
Dev-40 <44698972+Dev-40@users.noreply.github.com>
2018-11-30 15:08:55 +0200

    Overlaying Navigation Drawer - Over Action bar -

--- app/src/main/java/com/dev40/foodie/Category_Activity.java
+++ app/src/main/java/com/dev40/foodie/Category_Activity.java
@@ -3,20 +3,23 @@ package com.dev40.foodie;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class Category_Activity extends AppCompatActivity {
     private Button jams,appitizers,pickles,desserts,pastries,salads,dairy,mDishes;
     private Intent intent;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
 
 
     @Override
@@ -31,8 +34,10 @@ public class Category_Activity extends AppCompatActivity {
         salads = (Button) findViewById(R.id.salads_btn);
         dairy = (Button) findViewById(R.id.dairy_btn);
         mDishes = (Button) findViewById(R.id.mdishes_btn);
-        //for Navigation bar
 
+        //for Navigation bar
+        toolbar = (Toolbar)findViewById(R.id.nav_action);
+        setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
